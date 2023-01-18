e148894ffd46c453e9e0f203854a3235e906502e
HannahJacqueline <itshannahjacqueline@gmail.com>
2023-01-18 16:28:54 +0800

    link root route to app

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const ejs = require("ejs");
+const crypto = require("node:crypto");
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser(process.env.SECRET || "secret"));
+
+if (!process.env.SECRET) process.env.SECRET = crypto.randomBytes(28).toString("base64");
+app.use(cookieParser(process.env.SECRET));
+
+//routes
+app.use("/", require("./routes/root"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
