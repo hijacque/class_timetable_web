e6d877d2afe8df158e409ce78b02818ef8859636
HannahJacqueline <itshannahjacqueline@gmail.com>
2023-02-23 01:59:31 +0800

    modify argument for database connection

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
-const crypto = require("node:crypto");
 const { AppDatabase } = require("./lib/app-database");
 const { AppMailer } = require("./lib/app-mailer");
 require("dotenv").config();
@@ -12,19 +11,28 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-if (!process.env.SECRET) process.env.SECRET = crypto.randomBytes(28).toString("base64");
-app.use(cookieParser(process.env.SECRET));
+app.use(cookieParser(process.env.COOKIE_KEY));
 
 // local app variables
-app.locals.database = new AppDatabase(config.DB_PORT, config.DB_USER, config.DB_PASSWORD, config.DB_NAME);
+app.locals.database = new AppDatabase(config.DB_PORT, config.DB_USER, config.DB_PASSWORD, config.DB_NAME, true);
 app.locals.mailer = new AppMailer(
     config.SMTP, config.SMTP_PORT, config.SMTP_SENDER, config.SMTP_PASSWORD,
     "http://localhost:3000", "JAVAwokeez Team"
 );
 
-//routes
+// routes
 app.use("/", require("./routes/root"));
+app.use("/help", require("./routes/help"));
+app.use("/api", require("./routes/api"));
 
+// test
+// const crypto = require("node:crypto");
+app.get("/test", (req, res) => {
+    res.status(200).json(req.cookies);
+});
+// console.log(crypto.randomBytes(30).toString("base64"));
+
+// initialize server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log("Server listening on port " + port);
