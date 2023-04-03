968a1b53e3647b55f07f9e9f847286bc6200d3a2
HannahJacqueline <itshannahjacqueline@gmail.com>
2023-04-03 16:27:41 +0800

    modfy editing view for faculty schedules

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,13 +1,23 @@
 const router = require("express").Router();
+const { getSchedules } = require("./../lib/schedule");
 const { verifySession } = require("./../lib/verification");
 
 router.use(verifySession);
 
 router.get("/new", (req, res) => {
     if (!req.account) {
-        res.status(401).send("Nope!!!");
+        res.status(401).redirect("/login");
     }
-    res.status(200).send("Will create more on this");
+    res.status(200).render("schedule/new");
+});
+
+router.get("/:category", getSchedules, (req, res) => {
+    // TODO: check if category param is faculty of a valid course id in the department
+    res.render("schedule-root/base", {
+        category: req.params.category,
+        deptClasses: req.deptClasses, 
+        otherClasses: req.otherClasses
+    });
 });
 
 module.exports = router;
