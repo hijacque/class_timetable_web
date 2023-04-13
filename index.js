479cc34683f8997e16b09d591557fb0b455cf6d3
HannahJacqueline <itshannahjacqueline@gmail.com>
2023-04-13 20:14:14 +0800

    integrate ngrok for test deployment

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { AppDatabase } = require("./lib/app-database");
 const { AppMailer } = require("./lib/app-mailer");
+const ngrok = require("ngrok");
 require("dotenv").config();
 const config = process.env;
 
@@ -11,13 +12,6 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.COOKIE_KEY));
-
-// local app variables
-app.locals.database = new AppDatabase(config.DB_PORT, config.DB_USER, config.DB_PASSWORD, config.DB_NAME, true);
-app.locals.mailer = new AppMailer(
-    config.SMTP, config.SMTP_PORT, config.SMTP_SENDER, config.SMTP_PASSWORD,
-    "http://localhost:3000", "JAVAwokeez Team"
-);
 
 // routes
 app.use("/", require("./routes/root"));
@@ -32,7 +26,23 @@ app.get("/test", (req, res) => {
 app.get("*", (req, res) => res.status(404).send("You don't have to go back, but you can't stay here"));
 
 // initialize server
-const port = config.PORT || 3000;
+const port = config.API_PORT || 3000;
+// app.listen()
 app.listen(port, () => {
     console.log("Server listening on port " + port);
 });
+
+// ngrok.connect({ proto: "http", addr: port }, (err, url) => {
+//     config.API_DOMAIN = url;
+//     if (err) {
+//         console.error('Error while connecting Ngrok',err);
+//         return new Error('Ngrok Failed');
+//     }
+// });
+
+// local app variables
+app.locals.database = new AppDatabase(config.DB_PORT, config.DB_USER, config.DB_PASSWORD, config.DB_NAME, true);
+app.locals.mailer = new AppMailer(
+    config.SMTP, config.SMTP_PORT, config.SMTP_SENDER, config.SMTP_PASSWORD,
+    config.API_DOMAIN, "JAVAwokeez Team"
+);
