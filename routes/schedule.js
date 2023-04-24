e1b81f6244439a53dccdb2d3a3909cd58ec7e29a
HannahJacqueline <itshannahjacqueline@gmail.com>
2023-04-24 19:22:32 +0800

    integrate faculty load and scheduling genetic algo

--- routes/schedule.js
+++ routes/schedule.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { getSchedules, initSchedGen } = require("./../lib/schedule");
+const { getFacultySched, getBlockSched, generateSchedule } = require("./../lib/schedule");
 const { verifySession } = require("./../lib/verification");
 
 router.use(verifySession);
@@ -11,15 +11,37 @@ router.get("/new", (req, res) => {
     res.status(200).render("schedule/new");
 });
 
-router.post("/generate", initSchedGen, (req, res) => {
-    console.log(req.body);
-    res.send("Generating department's schedule");
+router.post("/generate", generateSchedule, (req, res) => {
+    res.status(200).redirect('/schedule/success/' + req.body.term);
 });
 
-router.get("/:category", getSchedules, (req, res) => {
+router.get('/success/:term', (req, res) => {
+    res.send(
+        `Successfully generated schedule. Go back to <a href="/chair/schedules?term=${req.params.term}">Schedules Tab</a>`
+    );
+});
+
+router.get('/failed/:term', (req, res) => {
+    res.send(
+        `Oh no! Something went wrong on our end, please retry later.<br>` +
+        `Go back to <a href="/chair/schedules?term=${req.params.term}">Schedules Tab</a>`
+    );
+});
+
+router.get("/faculty", getFacultySched, (req, res) => {
     // TODO: check if category param is faculty of a valid course id in the department
     res.render("schedule-root/base", {
-        category: req.params.category,
+        category: "faculty",
+        term: req.term,
+        schedule: req.data
+    });
+});
+
+router.get("/:blockID", getBlockSched, (req, res) => {
+    console.log("Viewing block schedule...");
+    // TODO: check if category param is faculty of a valid course id in the department
+    res.render("schedule-root/base", {
+        category: req.params.blockID,
         term: req.term,
         schedule: req.data
     });
