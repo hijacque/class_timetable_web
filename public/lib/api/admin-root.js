7377d84835629766a740e4a9eb641b713d53865d
HannahJacqueline <itshannahjacqueline@gmail.com>
2023-05-09 20:53:15 +0800

    separate admin api calls in departments tab

---
+++ public/lib/api/admin-root.js
@@ -0,0 +1,33 @@
+const getDepartments = (collegeID) => {
+    return new Promise((resolve, reject) => {
+        $.get("http://localhost:3000/api/departments/" + collegeID,
+            (data) => resolve(data.departments), "json"
+        ).fail((error) => {
+            console.log(error);
+            reject(error);
+        });
+    })
+};
+
+const addDepartment = (collegeID, deptData) => new Promise((resolve, reject) => $.post(
+    "http://localhost:3000/api/departments/" + collegeID, deptData,
+    (success) => resolve(success.message),
+    "json"
+).fail((error) => reject(error)));
+
+const addCollege = (name) => new Promise((resolve, reject) => $.post(
+    "http://localhost:3000/api/colleges", { name: name },
+    (data) => resolve(data.message),
+    "text json"
+).fail((error) => reject(error)));
+
+const getFaculty = (departmentID, columns) => new Promise((resolve, reject) => {
+    $.get("http://localhost:3000/api/faculty/" + departmentID, { columns: columns },
+        (data) => resolve(data.faculty), "json"
+    ).fail((data) => reject(data));
+});
+
+const updateDepartment = (deptID, newDeptData) => new Promise((resolve, reject) => $.post(
+    "http://localhost:3000/api/department/" + deptID, newDeptData,
+    (data) => resolve(data.message), "text json"
+).fail(error => reject(error)));
