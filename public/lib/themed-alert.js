7cdf3b5d09d855040d25bcce106067286e123bd2
HannahJacqueline <itshannahjacqueline@gmail.com>
2023-05-18 18:10:05 +0800

    fix schedule view when in saved mode

--- public/lib/themed-alert.js
+++ public/lib/themed-alert.js
@@ -7,6 +7,8 @@ class ThemedAlert {
         this.modal = modal;
     }
 
+    show() { this.modal.show(); }
+
     show(mode, title = "New Message", body = "", redirect) {
         let alertTitle = this.alert.find("#alert-title").removeClass(
             "text-danger text-success text-warning text-info"
