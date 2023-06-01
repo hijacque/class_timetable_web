39ce885cd0d23cd569e8357b2672811a04628e50
HannahJacqueline <itshannahjacqueline@gmail.com>
2023-06-01 17:16:04 +0800

    fix time data presentation - separate utility function for converting time and week day

--- public/lib/display-dropdown.js
+++ public/lib/display-dropdown.js
@@ -1,11 +1,12 @@
 function displayDropMenu(dropMenuBtn, origLabel) {
     let dropList = dropMenuBtn.next("ul.dropdown-menu");
-    let dropItems = dropList.find("a.dropdown-item");
+    let dropItems = dropList.find(".dropdown-item");
     
     dropItems.click((event) => {
-        const value = event.currentTarget.textContent;
-        if (value.toLowerCase() == "cancel") {
-            dropMenuBtn.html(origLabel).val(null);
+        console.log(dropMenuBtn, origLabel);
+        const value = event.currentTarget.textContent.trim();
+        if (value.toLowerCase() == "cancel" || $(event.currentTarget).hasClass("cancel")) {
+            dropMenuBtn.text(origLabel);
         } else {
             dropMenuBtn.val(value).text(value);
         }
