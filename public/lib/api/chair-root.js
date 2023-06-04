3bc5bf4c3482f969a87f704256acc232fa058564
HannahJacqueline <itshannahjacqueline@gmail.com>
2023-06-05 00:11:32 +0800

    fix chairoerson api call and faculty table - integrate update faculty data function

--- public/lib/api/chair-root.js
+++ public/lib/api/chair-root.js
@@ -1,8 +1,8 @@
-const addTerm = (year, semester) => new Promise((resolve, reject) => $.post(
+const addTerm = (year, semester) => new Promise((resolve) => $.post(
     "/api/terms", { year: year, term: semester },
     (data) => {
         resolve(data.termID);
-    }).fail((data) => location.reload())
+    }).fail(() => location.reload())
 );
 
 const addBlock = (courseID, year, totalStudents, termID) => new Promise((resolve, reject) => {
@@ -25,15 +25,13 @@ const addCourse = (title) => new Promise((resolve, reject) => $.post(
 ).fail((data) => reject(data)));
 
 const getCurriculum = (courseID) => new Promise((resolve, reject) => {
-    $.get("/api/curricula/" + courseID,
-        (data) => { resolve(data) }, "json"
+    $.get("/api/curricula/" + courseID, (data) => resolve(data)
     ).fail((error) => reject(error))
 });
 
 const addSemester = (courseID, semData) => $.post(
-    "/api/curricula/" + courseID, semData,
-    (data) => console.log(data)
-).fail((data) => console.log(data));
+    "/api/curricula/" + courseID, semData, (data) => resolve(data)
+).fail((error) => reject(error.responseJSON));
 
 const addCourseSubject = (courseID, subjData) => new Promise((resolve, reject) => {
     $.post("/api/curriculum/" + courseID, subjData,
